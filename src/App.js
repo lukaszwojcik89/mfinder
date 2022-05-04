9514b41e1291de8cb8921e1a737b40591d9bb6a9
Łukasz Wójcik <wojcik.l@tlen.pl>
2022-05-04 20:20:40 +0200

    blad

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <form action="">
-        <label htmlFor="">
-          Search:
-        </label>
-        <input type="text" id="search"/>
-        <button type="submit">Find movie</button>
-      </form>
-    </div>
-  );
+    const [value, setValue] = useState('');
+
+    async function getMovieData(query) {
+        const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=ab99dc29cd462eba2f2e028c132ea64c&language=pl-PL&query=${query}&page=1&include_adult=true`)
+
+        if (response.status !== 200) {
+            throw new Error('Error: ' + 'Something is no yes');
+        }
+        const data = await response.json();
+        return data.results;
+    }
+
+    function handleChange(event) {
+        setValue(event.target.value);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
+    return (
+        <div className="App">
+            <form action="" onSubmit={handleSubmit}>
+                <label htmlFor="search">
+                    Search:
+                </label>
+                <input type="text" id="search" value={value} onChange={handleChange} />
+                <button type="submit">Find</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
