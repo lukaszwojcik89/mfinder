e2bde8ee79c4600cae8a64867b86b9d4d48aabeb
Łukasz Wójcik <wojcik.l@tlen.pl>
2022-05-04 22:10:14 +0200

    working api for finding movies -v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, setValue] = useState('');
+    const [movies, setMovies] = useState([]);
+
+    async function getMovieData(query) {
+        const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=ab99dc29cd462eba2f2e028c132ea64c&language=pl-PL&query=${query}&page=1&include_adult=true`)
+
+        if (response.status !== 200) {
+            throw new Error('Error: ' + 'Something is no yes');
+        }
+        const data = await response.json();
+        return data.results;
+    }
+
+    function handleChange(event) {
+        setValue(event.target.value);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        getMovieData(value).then((data) => {
+            setMovies(data);
+        })
+    }
+
+    return (
+        <div className="App">
+            <form action="" onSubmit={handleSubmit}>
+                <label htmlFor="search">
+                    Search:
+                </label>
+                <input type="text" id="search" value={value} onChange={handleChange} />
+                <button type="submit">Find</button>
+            </form>
+
+            {/* <pre>
+                {JSON.stringify(movies, null, 2)}
+            </pre> */}
+            {movies.map((movie) => (
+                <div key={movie.id} style={{ 'border': '1px solid lightcoral', 'borderRadius': '15px', 'padding': '10px', 'margin': '10px' }}>
+                    <h2>{movie.title}</h2>
+                    {movie.poster_path ? (
+                        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} style={{ 'width': '100%' }} alt={`Poster of ${movie.title}`} />
+                    ) : (
+                        <h4>Something is no yes</h4>
+                    )}
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default App;
